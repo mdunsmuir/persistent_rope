171d02d511626cd325f3fcf0b51c3d8e55139a92
Michael Dunsmuir <mjdunsmuir@gmail.com>
2016-07-25 20:18:02 -0700

    updated tests a bit

--- src/tests.rs
+++ src/tests.rs
@@ -1,26 +1,69 @@
 use super::*;
 
-fn sample_rope() -> Rope<usize> {
-    let v1 = vec![1, 2, 3];
-    let v2 = vec![4, 5, 6];
-    let v3 = vec![7, 8, 9];
+pub fn sample_flat_rope() -> Rope<usize> {
+    Rope::new(vec![0, 1, 2])
+}
+
+pub fn sample_deep_rope() -> Rope<usize> {
+    let v1 = vec![0, 1, 2];
+    let v2 = vec![3, 4, 5];
+    let v3 = vec![6, 7, 8];
 
     Rope::concat(Rope::new(v1), Rope::concat(Rope::new(v2), Rope::new(v3)))
 }
 
-#[test]
-fn basic_indexing() {
-    let rope = sample_rope();
-    for i in 1..9 {
-        assert_eq!(i, *rope.at(i - 1));
+mod indexing {
+
+    use super::*;
+
+    #[test]
+    fn flat() {
+        let rope = sample_flat_rope();
+        for i in 0..2 {
+            assert_eq!(i, *rope.at(i));
+        }
+    }
+
+    #[test]
+    fn deep() {
+        let rope = sample_deep_rope();
+        for i in 0..8 {
+            assert_eq!(i, *rope.at(i));
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn flat_panic_on_out_of_bounds() {
+        sample_flat_rope().at(3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn deep_panic_on_out_of_bounds() {
+        sample_flat_rope().at(9);
     }
 }
 
-#[test]
-fn substring() {
-    let base = sample_rope();
-    let sub = base.substring(1, 5);
-    for i in 0..4 {
-        assert_eq!(base.at(i + 1), sub.at(i))
+mod substring {
+
+    use super::*;
+
+    /*
+    #[test]
+    fn flat() {
+        let base = sample_flat_rope();
+
+        
+    }
+    */
+
+    #[test]
+    fn deep() {
+        let base = sample_deep_rope();
+        let sub = base.substring(1, 5);
+        for i in 0..4 {
+            assert_eq!(base.at(i + 1), sub.at(i))
+        }
     }
 }
